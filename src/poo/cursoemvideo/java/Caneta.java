82a9d6938ccb36a86e235fa38055c599f4b834e6
Otavio <ottvferreira@gmail.com>
2022-07-14 15:46:22 -0300

    Instancia objeto c1

--- src/poo/cursoemvideo/java/Caneta.java
+++ src/poo/cursoemvideo/java/Caneta.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package poo.cursoemvideo.java;
 
-/**
- *
- * @author otvfe
- */
 public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
     
+    void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga + "%");
+        System.out.println("Tampada: " + this.tampada);
+    }
+    void rabiscar(){
+        
+    }
+    void tampar(){
+       this.tampada = true;
+    }
+    void destampar(){
+       this.tampada = false; 
+    }
 }
--- src/poo/cursoemvideo/java/POOCursoEmVideoJava.java
+++ src/poo/cursoemvideo/java/POOCursoEmVideoJava.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package poo.cursoemvideo.java;
 
-/**
- *
- * @author otvfe
- */
 public class POOCursoEmVideoJava {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Caneta c1 = new Caneta();
+        c1.modelo = "Aleatorio";
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.carga = 90;
+
+        c1.destampar();
+        c1.status();        
     }
     
 }

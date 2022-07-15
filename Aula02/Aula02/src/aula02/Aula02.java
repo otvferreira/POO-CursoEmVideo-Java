6c313b0d4ae2c5d51fa17489b707741088133247
Otavio <ottvferreira@gmail.com>
2022-07-15 12:38:11 -0300

    Adiciona Aula 02

---
+++ Aula02/Aula02/src/aula02/Aula02.java
@@ -0,0 +1,22 @@
+
+package aula02;
+
+
+public class Aula02 {
+
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "Bic";
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.carga = 90;
+        
+        c1.destampar();
+        c1.status();
+        
+        c1.rabiscar();
+        
+    }
+    
+}
---
+++ Aula02/Aula02/src/aula02/Caneta.java
@@ -0,0 +1,32 @@
+
+package aula02;
+
+
+public class Caneta {
+   String modelo;
+   String cor;
+   float ponta;
+   int carga;
+   boolean tampada;
+   
+   void status(){
+       System.out.println("Modelo: " + this.modelo);
+       System.out.println("Cor: " + this.cor);
+       System.out.println("Ponta: " + this.ponta);
+       System.out.println("Carga:" + this.carga);
+       System.out.println("Tampada: " + this.tampada);
+   }
+   void rabiscar(){
+       if (tampada == true){
+           System.out.println("ERRO! Caneta tampada.");
+       } else {
+           System.out.println("Rabiscando");
+       }
+   }
+   void tampar(){
+       this.tampada = true;
+   }
+   void destampar(){
+       this.tampada = false;
+   }
+}

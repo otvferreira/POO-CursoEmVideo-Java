4451cb158f9018d8473444369615fd93195845bc
Otavio <ottvferreira@gmail.com>
2022-07-15 20:32:19 -0300

    Atualiza Aula 03

--- Aula03/src/aula02/Caneta.java
+++ Aula03/src/aula02/Caneta.java
@@ -2,30 +2,30 @@ package aula02;
 
 
 public class Caneta {
-   String modelo;
-   String cor;
-   float ponta;
-   int carga;
-   boolean tampada;
+   public String modelo;
+   public String cor;
+   private float ponta;
+   protected int carga;
+   private boolean tampada;
    
-   void status(){
+   public void status(){
        System.out.println("Modelo: " + this.modelo);
        System.out.println("Cor: " + this.cor);
        System.out.println("Ponta: " + this.ponta);
        System.out.println("Carga: " + this.carga + "%");
        System.out.println("Tampada: " + this.tampada);
    }
-   void rabiscar(){
+   public void rabiscar(){
        if (tampada == true){
            System.out.println("ERRO! Caneta tampada.");
        } else {
            System.out.println("Rabiscando");
        }
    }
-   void tampar(){
+   protected void tampar(){
        this.tampada = true;
    }
-   void destampar(){
+   protected void destampar(){
        this.tampada = false;
    }
 }

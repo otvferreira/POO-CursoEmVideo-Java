c15ab8a7f0d181d87948e4afc8771e28723b4e66
Otavio <ottvferreira@gmail.com>
2022-07-17 15:44:10 -0300

    Finalização de classe 'Luta' e resultados gerados com java.util.Random

--- Aula08/src/aula07/Aula07.java
+++ Aula08/src/aula07/Aula07.java
@@ -23,11 +23,14 @@ public class Aula07 {
         l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);       
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
 
-        
+        /*
         for (Lutador lutador: l) {
         lutador.apresentar();
         lutador.status();
-}
+        }*/
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[0], l[1]);
+        UEC01.lutar();
     }
     
 }
--- Aula08/src/aula07/Luta.java
+++ Aula08/src/aula07/Luta.java
@@ -4,6 +4,8 @@
  */
 package aula07;
 
+import java.util.Random;
+
 /**
  *
  * @author otvfe
@@ -12,45 +14,82 @@ public class Luta {
     private Lutador desafiado;
     private Lutador desafiante;
     private int rounds;
-    private int aprovada;
+    private boolean aprovada;
     
-    public void marcarLuta(){
-
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if((l1.getCategoria().equals(l2.getCategoria())) && (l1 != l2)){
+            this.setAprovada(true);
+            this.setDesafiado(l1);
+            this.setDesafiante(l2);
+        } else {
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+            
+        }
     }
     
     public void lutar(){
-        
+        if (this.getAprovada()){
+
+            
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3);
+            
+            switch(vencedor){
+                case 0:
+                    System.out.println("Empate");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                break;
+                case 1:
+                    System.out.println("Vitoria Desafiado " + this.desafiado.getNome());
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                
+                break;
+                case 2:
+                    System.out.println("Vitoria Desafiante " + this.desafiante.getNome());
+                    this.desafiado.perderLuta();
+                    this.desafiante.ganharLuta();
+                break;
+            }
+            this.desafiado.apresentar();
+            this.desafiante.apresentar();
+        } else {
+            System.out.println("Luta não pode acontecer!");
+        }
     }
 
-    public Lutador getDesafiado() {
+    private Lutador getDesafiado() {
         return desafiado;
     }
 
-    public void setDesafiado(Lutador desafiado) {
+    private void setDesafiado(Lutador desafiado) {
         this.desafiado = desafiado;
     }
 
-    public Lutador getDesafiante() {
+    private Lutador getDesafiante() {
         return desafiante;
     }
 
-    public void setDesafiante(Lutador desafiante) {
+    private void setDesafiante(Lutador desafiante) {
         this.desafiante = desafiante;
     }
 
-    public int getRounds() {
+    private int getRounds() {
         return rounds;
     }
 
-    public void setRounds(int rounds) {
+    private void setRounds(int rounds) {
         this.rounds = rounds;
     }
 
-    public int getAprovada() {
+    private boolean getAprovada() {
         return aprovada;
     }
 
-    public void setAprovada(int aprovada) {
+    private void setAprovada(boolean aprovada) {
         this.aprovada = aprovada;
     }
     

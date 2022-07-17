ff62d2e44a11017ece20c6cdac007cb9247ab09c
Otavio <ottvferreira@gmail.com>
2022-07-17 15:09:31 -0300

    Adiciona classe Luta

---
+++ Aula08/src/aula07/Luta.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula07;
+
+/**
+ *
+ * @author otvfe
+ */
+public class Luta {
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private int aprovada;
+    
+    public void marcarLuta(){
+
+    }
+    
+    public void lutar(){
+        
+    }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public int getAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(int aprovada) {
+        this.aprovada = aprovada;
+    }
+    
+    
+    
+}
